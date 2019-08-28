c8c240f4f9f63d288eff635ad9bc64339f2db2b2
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-08-28 01:49:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,12 @@
 def square_array(array)
   counter = 0 
+  new_array = []
   
   while array[counter] do
-    puts (array[counter] ** 2)
+    num = array[counter] ** 2
+    new_array.push(num)
     counter += 1
   end
+  
+  p new_array
 end
